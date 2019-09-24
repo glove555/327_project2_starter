49923f22ea2ec2a399fb68f4895bc1c7260041a4
Gillian Love <gillian.love.16@cnu.edu>
2019-09-24 15:29:00 -0400

    Close file done; array created.

--- src/array_functions.cpp
+++ src/array_functions.cpp
@@ -29,47 +29,75 @@ struct track
 	int number_occ;
 };
 //TODO add a global array of entry structs (global to this file)
-
+track myTrack[constants::MAX_WORDS];
 //TODO add variable to keep track of next available slot in array
 int next;
 //TODO define all functions in header file
+//zero out array that tracks words and their occurrences
 void clearArray(){
 
 }
 
+//how many unique words are in array
 int getArraySize(){
 	return 0;
 }
+
+//get data at a particular location
 std::string getArrayWordAt(int i){
-	return " ";
+	return "";
 }
 int getArrayWord_NumbOccur_At(int i){
 	return 0;
 }
+
+/*loop through whole file, one line at a time
+ * call processLine on each line
+ * returns false: myfstream is not open
+ *         true: otherwise*/
 bool processFile(std::fstream &myfstream){
 	return false;
 }
+
+/*take 1 line and extract all the tokens from it
+feed each token to processToken for recording*/
 void processLine(std::string &myString){
 
 }
+
+/*Keep track of how many times each token seen*/
 void processToken(std::string &token){
 
 }
+
+/*if you are debugging the file must be in the project parent directory
+  in this case Project2 with the .project and .cProject files*/
 bool openFile(std::fstream& myfile, const std::string& myFileName,
 		std::ios_base::openmode mode){
-
-	myfile.open(myFileName.c_str());
-	if (!myfile.is_open())
-		return false;
-
 	return true;
 }
+
+/*if myfile is open then close it*/
 void closeFile(std::fstream& myfile){
-	myfile.close();
+	if (myfile.is_open()){
+		myfile.close();
+	}
 }
+
+/* serializes all content in myEntryArray to file outputfilename
+ * returns  FAIL_FILE_DID_NOT_OPEN if cannot open outputfilename
+ * 			FAIL_NO_ARRAY_DATA if there are 0 entries in myEntryArray
+ * 			SUCCESS if all data is written and outputfilename closes OK
+ * */
 int writeArraytoFile(const std::string &outputfilename){
 	return 0;
 }
+
+/*
+ * Sort myEntryArray based on so enum value.
+ * You must provide a solution that handles alphabetic sorting (A-Z)
+ * The presence of the enum implies a switch statement based on its value
+ */
 void sortArray(constants::sortOrder so){
 
 }
